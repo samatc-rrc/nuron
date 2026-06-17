986d35d00bc5d847f10bfaabfded3d31090e64b3
samatc.rrc <samatc.rrc@gmail.com>
2026-06-17 22:40:32 +0700

    style: set IBM Plex Sans Thai as main font, Noto Sans Thai fallback

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@400;500;600;700&family=Noto+Sans+Thai:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
